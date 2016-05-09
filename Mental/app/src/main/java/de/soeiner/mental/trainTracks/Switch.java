cfddd1f2efcf4c5526bb983d85c39439faa8ba41
m1a9l9t7e <m1a9l9t7e@gmail.com>
2016-05-09 16:28:29 +0200

    communication update

--- Mental/app/src/main/java/de/soeiner/mental/trainTracks/Switch.java
+++ Mental/app/src/main/java/de/soeiner/mental/trainTracks/Switch.java
@@ -1,9 +1,12 @@
 package de.soeiner.mental.trainTracks;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+
+import de.soeiner.mental.gameModes.Train;
 
 /**
  * Created by Malte on 26.04.2016.
@@ -32,11 +35,16 @@ public class Switch extends TrainTrack {
         if (!successors.contains(s)) {
             successors.add(s);
             activeSuccessors.add(false);
-            JSONObject position = new JSONObject();
+            JSONArray successorList = new JSONArray();
             try {
-                position.put("xpos", s.getX());
-                position.put("ypos", s.getY());
-                this.put("succesorNo_"+successors.size()+"Position", position);
+                for(int i = 0; i < successors.size(); i++){
+                    TrainTrack temp =  successors.get(i);
+                    JSONObject position = new JSONObject();
+                    position.put("xpos", temp.getX());
+                    position.put("ypos", temp.getY());
+                    successorList.put(i, position);
+                }
+                this.put("successorList", successorList);
             }catch(JSONException e){e.printStackTrace();}
         }
         changeSwitch(); //switch gleich setzen
@@ -59,6 +67,9 @@ public class Switch extends TrainTrack {
                 successor = successors.get(i);
             }
         }
+        try {
+            this.put("switchedTo", active);
+        }catch(Exception e){e.printStackTrace();}
     }
 
     public ArrayList getSuccessors(){
