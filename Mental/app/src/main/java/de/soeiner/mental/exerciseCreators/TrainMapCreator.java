81aeb7a04f9d5efb2fba83499f86aac6ba266338
m1a9l9t7e <m1a9l9t7e@gmail.com>
2016-05-19 16:57:11 +0200

    map um blockedtracks verkleinert
    ich kann diese anderen files irgendwie nicht ignoren

--- Mental/app/src/main/java/de/soeiner/mental/exerciseCreators/TrainMapCreator.java
+++ Mental/app/src/main/java/de/soeiner/mental/exerciseCreators/TrainMapCreator.java
@@ -190,8 +190,14 @@ public class TrainMapCreator extends ExerciseCreator {
         }
         System.out.println("map: ");
         ausgabe();
-        this.trainMap = map;
-        return map;
+        TrainTrack[][] finalMap = new TrainTrack[size-2][size-2];
+        for(int i = 1; i<size-1; i++) {
+            for (int j = 1; j < size - 1; j++) {
+                finalMap[i-1][j-1] = map[i][j];
+            }
+        }
+        this.trainMap = finalMap;
+        return finalMap;
     }
 
     private int[] getStartingPoint() {
